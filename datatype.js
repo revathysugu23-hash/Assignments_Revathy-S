686254f1eea917502ba4802c598760a554ba9e90
revathysugu23-hash <revathysugu23@gmail.com>
2025-12-06 17:58:55 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,14 @@
+//browseVersion ->Number
+//browsername->String
+//isBrowserOpen->boolean
+//history->null(typeof)
+///print the value and typeof the variable 
+var browserVersion=143;
+var browsername ='Chrome';
+var isBrowserOpen= true;
+var history=null;
+console.log(+browserVersion);
+
+console.log(+browsername);
+console.log (isBrowserOpen);
+console.log(typeof history);
